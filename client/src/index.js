bec9731c7842868377cc57dc892c2cd8d7089687
D4_62857_Shubham_Pisal <pisalshubham3@gmail.com>
2022-07-09 16:45:59 +0530

    added routes to index file

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import First from './pages/First'
+import SignIn from './pages/Account/SignIn'
+import SignUp from './pages/Account/SignUp'
+import ForgotPassword from './pages/Account/ForgotPassword'
+import ChangePassword from './pages/Account/ChangePassword'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import EditProfile from './pages/Account/EditProfile'
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<First />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/forgotpassword' element={<ForgotPassword />} />
+        <Route path='/changepassword' element={<ChangePassword />} />
+        <Route path='/editprofile' element={<EditProfile />} />
+        <Route path='/' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
