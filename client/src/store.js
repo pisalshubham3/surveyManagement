cb1d4fd4229f445af851c7ff7b34f3e3f9c9e262
D4_62857_Shubham_Pisal <pisalshubham3@gmail.com>
2022-07-09 18:55:08 +0530

    renamed store file

---
+++ client/src/store.js
@@ -0,0 +1,31 @@
+import storage from 'redux-persist/lib/storage'
+import authSlice from './slices/authSlice'
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+const persistConfig = {
+  key: 'counter',
+  storage,
+}
+const reducers = combineReducers({ authSlice })
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+export default store
